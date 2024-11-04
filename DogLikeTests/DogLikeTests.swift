55afb92bd3b9e9bca94d0a1942d8a29d1d6c47cb
Markus Wirtz <info@markuswirtz.net>
2024-11-04 16:07:02 +0100

    Datensicherung Neone

--- DogLikeTests/DogLikeTests.swift
+++ DogLikeTests/DogLikeTests.swift
@@ -10,27 +10,46 @@ import XCTest
 
 @MainActor
 final class DogLikeTests: XCTestCase {
-    private var viewModel: DogViewModel?
-    private var greetingName: String?
-
+    var viewModel: DogViewModel?
+    var greetingName: String?
+    var breedUrl: String?
+    
     override func setUpWithError() throws {
         viewModel = DogViewModel()
         greetingName = "Jürgen"
+        breedUrl = "https://images.dog.ceo/breeds/pembroke/n02113023_2330.jpg"
     }
     override func tearDownWithError() throws {
         viewModel = nil
         greetingName = nil
+        breedUrl = nil
     }
     
     func testExample() {}
-
-    func greetingsTest() {
+    
+    func testGreetingEqual() {
         XCTAssertEqual(viewModel?.greeting(name: greetingName!), "Welcome to DogLike, Jürgen!")
     }
+    
+    func testBreedNameIsEqual() {
+        XCTAssertEqual(viewModel?.extractBreedName(from: breedUrl!), "pembroke")
+    }
+    
+    func testBreedNameIsNotEqual() {
+        XCTAssertNotEqual(viewModel?.extractBreedName(from: breedUrl!), "test")
+    }
+    
+    func testBreedNameIsNotNil() {
+        XCTAssertNotNil(viewModel?.extractBreedName(from: breedUrl!))
+    }
+    
+//    func textBreedNameisNil() {
+//        XCTAssertNil(viewModel?.extractBreedName(from: nil))
+//    }
     
     func testPerformanceExample() throws {
         measure {
             XCTAssertEqual(viewModel?.greeting(name: greetingName!), "Welcome to DogLike, Jürgen!")
         }
-    } 
+    }
 } // End Class
